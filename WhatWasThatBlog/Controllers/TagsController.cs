4c81350bbfdf1c26aba5415adb3c9342251ef99c
TriciaCArt <illiannamama@gmail.com>
2022-06-16 21:14:24 -0400

    Some changes to pictures and the controller

--- WhatWasThatBlog/Controllers/TagsController.cs
+++ WhatWasThatBlog/Controllers/TagsController.cs
@@ -1,11 +1,6 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WhatWasThatBlog.Data;
 using WhatWasThatBlog.Models;
